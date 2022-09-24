d5f544c85973cf7ed5e7a23ce8eb16393d241e64
skabuamer <shaikhabuamer@gmail.com>
2022-09-25 01:33:55 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var slider = tns({
+    container: ".clients",
+    items: 5,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+});
